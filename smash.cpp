82d7c903f2f8a980f6c510ae36ef335de5f0814e
drorco92 <57985817+drorco92@users.noreply.github.com>
2020-11-30 18:23:17 +0200

    timeout

--- smash.cpp
+++ smash.cpp
@@ -26,9 +26,11 @@ int main(int argc, char* argv[]) {
     while(true) {
         std::cout << smash.promptDisplay();
         std::string cmd_line;
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max());
+        std::cin.clear();
+        std::fflush(stdin);
         std::getline(std::cin, cmd_line);
         smash.executeCommand(cmd_line.c_str());
+
     }
     return 0;
 }
