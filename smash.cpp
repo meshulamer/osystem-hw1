4c586bd68b4501033569b81c620182362019853f
drorco92 <57985817+drorco92@users.noreply.github.com>
2020-11-30 15:56:42 +0200

    timeout

--- smash.cpp
+++ smash.cpp
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <limits>
 #include "Commands.h"
 #include "signals.h"
 
@@ -25,6 +26,7 @@ int main(int argc, char* argv[]) {
     while(true) {
         std::cout << smash.promptDisplay();
         std::string cmd_line;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max());
         std::getline(std::cin, cmd_line);
         smash.executeCommand(cmd_line.c_str());
     }
